758ded78786617f17c22a1296e89013daa775993
Hoonter <huntdawghero@gmail.com>
2023-12-09 17:06:20 -0700

    Added utility file and debounce function

---
+++ src/utility/m4_utility.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <stdint.h>
+#include "m4_utility.hpp"
+
+bool debounce(uint32_t pin){
+    bool state;
+    state = digitalRead(pin);
+    delay(30);
+    if (state == digitalRead(pin)){
+        return true;   
+    }
+    else{
+        return false;
+    }
+}
---
+++ src/utility/m4_utility.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdint.h>
+
+bool debounce(uint32_t pin);
