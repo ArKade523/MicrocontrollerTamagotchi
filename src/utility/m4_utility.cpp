417e9219b8f661a15b925fb5a70cc2b0e1bf9fdc
Hoonter <huntdawghero@gmail.com>
2023-12-09 18:21:13 -0700

    Added buttonPushed functions to utility

--- src/utility/m4_utility.cpp
+++ src/utility/m4_utility.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <stdint.h>
+#include "globals.h"
 #include "m4_utility.hpp"
 
 bool debounce(uint32_t pin){
@@ -12,4 +13,32 @@ bool debounce(uint32_t pin){
     else{
         return false;
     }
+}
+
+bool leftButtonPushed(){
+    if (digitalRead(LEFT_BUTTON) == LOW){
+        if (debounce(LEFT_BUTTON)){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool rightButtonPushed(){
+    if (digitalRead(RIGHT_BUTTON) == LOW){
+        if (debounce(RIGHT_BUTTON)){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool centerButtonPushed(){
+    if (digitalRead(CENTER_BUTTON) == LOW){
+        if (debounce(CENTER_BUTTON)){
+            return true;
+        }
+    }
+    return false;
 }
--- src/utility/m4_utility.hpp
+++ src/utility/m4_utility.hpp
@@ -3,3 +3,6 @@
 #include <stdint.h>
 
 bool debounce(uint32_t pin);
+bool leftButtonPushed();
+bool rightButtonPushed();
+bool centerButtonPushed();
